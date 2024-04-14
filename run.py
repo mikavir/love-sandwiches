5dd071feb26a7f409042fd96795e6ba99a8e888c
Mika Virtucio <mikavirtucio@gmail.com>
2024-04-14 12:47:06 +0000

    Define main function

--- run.py
+++ run.py
@@ -61,7 +61,14 @@ def update_sales_worksheet(data):
 
 
   
+def main():
+    """
+    Runs all program functions
+    """
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+
+print("Welcome to Love Sandwiches Data Automation")
+main()
